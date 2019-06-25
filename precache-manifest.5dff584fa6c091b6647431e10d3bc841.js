2222353cd868bd1551969cda75f19fbb53294ba9
Misha C <cn0073@CN0073.lan>
2019-06-25 13:48:17 +0100

    Updates

---
+++ precache-manifest.5dff584fa6c091b6647431e10d3bc841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeabfb63c93549d3e1f79ca8722e2ea4",
+    "url": "/Sellar-Door/counter2.git/index.html"
+  },
+  {
+    "revision": "2e3e65623a56b6371abb",
+    "url": "/Sellar-Door/counter2.git/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Sellar-Door/counter2.git/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2e3e65623a56b6371abb",
+    "url": "/Sellar-Door/counter2.git/static/js/main.93cb1b08.chunk.js"
+  },
+  {
+    "revision": "d1e0efe659dc90b817af",
+    "url": "/Sellar-Door/counter2.git/static/js/runtime~main.c6baba4a.js"
+  }
+]);
